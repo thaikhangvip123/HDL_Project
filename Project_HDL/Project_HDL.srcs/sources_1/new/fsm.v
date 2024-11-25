07301176a54387ae9a7e24128eb12169d31a5b57
DESKTOP-JK6HBSK\thaik <thaikhangvip123@gmail.com>
2024-11-25 21:30:05 +0700

    Init begin project

---
+++ Project_HDL/Project_HDL.srcs/sources_1/new/fsm.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/25/2024 09:18:16 PM
+// Design Name: 
+// Module Name: fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fsm(
+    input   [3:0] in,
+    output  [3:0] out
+    );
+    assign out = in;
+endmodule
